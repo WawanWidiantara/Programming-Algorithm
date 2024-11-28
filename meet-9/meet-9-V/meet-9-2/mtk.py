39f41451f13cff548a5fbf2d8b6ba586c9835bf1
wawanwidiantara <gdwidi13@gmail.com>
2024-11-28 09:12:08 +0700

    add: implement basic arithmetic functions in matematika and fisika modules

---
+++ meet-9/meet-9-V/meet-9-2/mtk.py
@@ -0,0 +1,20 @@
+# Penjumlahan, pengurangan, perkalian, pembagian
+
+def penjumlahan(angka1, angka2):
+    hasil = angka1 + angka2
+    return hasil
+
+
+def pengurangan(angka1, angka2):
+    hasil = angka1 - angka2
+    return hasil
+
+
+def perkalian(angka1, angka2):
+    hasil = angka1 * angka2
+    return hasil
+
+
+def pembagian(angka1, angka2):
+    hasil = angka1 / angka2
+    return hasil
