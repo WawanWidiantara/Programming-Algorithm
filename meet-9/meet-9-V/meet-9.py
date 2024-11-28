1020dfc69ca3450656b606e17fba35671b8f2517
wawanwidiantara <gdwidi13@gmail.com>
2024-11-28 07:46:56 +0700

    add examples for dictionary data manipulation including retrieval, update, and deletion

---
+++ meet-9/meet-9-V/meet-9.py
@@ -0,0 +1,36 @@
+# Dictionary {} -> "key" -> matematika, "value" -> ilmu yang mempelajari ...
+mahasiswa = {
+    "nama": "Widi",
+    "npm": 134,
+    "alamat": "jl",
+    "asal": "Bali",
+}
+
+# # Cara mengambil data dictionary
+# ? langsung
+print(f"Pemanggilan secara langsung: {mahasiswa['nama']}")
+# ? dengan get() -> get(key, default value if not found)
+print(mahasiswa.get("ip", "Key not found"))
+
+# Cara mengupdate/insert data
+#! key "ip" val 4
+# secara langsung
+mahasiswa["ip"] = 4
+mahasiswa["ip"] = 3
+print(mahasiswa)
+
+# dengan update()
+mahasiswa.update({"ip": 4})
+mahasiswa.update({"ip": 3})
+print(mahasiswa)
+
+# menghapus data dictionary
+# dengan del
+del mahasiswa["nama"]
+print(mahasiswa)
+# dengan pop
+mahasiswa.pop("alamat")
+print(mahasiswa)
+# menghilangkan nilai terakhir
+mahasiswa.popitem()
+print(mahasiswa)
