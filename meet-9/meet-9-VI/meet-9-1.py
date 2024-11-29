62df58d1b48d2f62705d8355326f52e34196f2fe
wawanwidiantara <gdwidi13@gmail.com>
2024-11-29 07:45:53 +0700

    add: enhance dictionary example with data update and insertion methods

--- meet-9/meet-9-VI/meet-9-1.py
+++ meet-9/meet-9-VI/meet-9-1.py
@@ -9,9 +9,10 @@ mahasiswa = {
     "nama": "Widi",
     "npm": 135,
     "asal": "Bali",
+    # "ip" : 4.0
 }
 
-# Mengambil data pada dictionary
+# ? Mengambil data pada dictionary
 #! secara langsung
 print(mahasiswa["npm"])
 # print(mahasiswa["ip"]) #! should be error
@@ -21,3 +22,25 @@ print(f"Dengan get(): {mahasiswa.get('npm')}")
 print(f"Dengan get('ip'): {mahasiswa.get('ip')}")
 print(f"Dengan get('ip'), dengan default: {mahasiswa.get('ip','Key not found')}")
 
+# ? Mengupdate/insert data ke dictionary
+#! secara langsung
+mahasiswa["nama"] = "Ilham"  # cara update
+print(f"Update langsung: {mahasiswa}")
+mahasiswa["ip"] = 4.0  # insert baru
+print(f"Insert langsung: {mahasiswa}")
+
+#! dengan update()
+mahasiswa.update(
+    {
+        "npm": 101,
+        "asal": "Ciamis",
+    }
+)
+print(f"Update dengan update(): {mahasiswa}")
+mahasiswa.update(
+    {
+        "hobi": ["ngoding", "membaca"],
+        "dob": "13-08-2002",
+    }
+)
+print(f"Insert dengan update(): {mahasiswa}")
